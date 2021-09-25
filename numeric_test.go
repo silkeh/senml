ddc8281cc6063859963114066547c94681cf4fb8
Silke Hofstra <silke@slxh.eu>
2021-09-25 16:48:55 +0200

    Add XML encoding tests for Numeric types

--- numeric_test.go
+++ numeric_test.go
@@ -1,6 +1,8 @@
 package senml
 
 import (
+	"encoding/xml"
+	"fmt"
 	"math"
 	"testing"
 )
@@ -10,26 +12,27 @@ type numericTest struct {
 	F float64
 	I int64
 	U uint64
+	S string
 }
 
 var numericTests = []numericTest{
-	{N: uint(5), F: 5, I: 5, U: 5},
-	{N: uint8(44), F: 44, I: 44, U: 44},
-	{N: uint16(11290), F: 11290, I: 11290, U: 11290},
-	{N: uint32(739910425), F: 739910425, I: 739910425, U: 739910425},
-	{N: uint64(3177891079421588183), F: 3177891079421588183, I: 3177891079421588183, U: 3177891079421588183},
-	{N: int(-6), F: -6, I: -6, U: math.MaxUint64 - 5},
-	{N: int8(-45), F: -45, I: -45, U: math.MaxUint64 - 44},
-	{N: int16(-11291), F: -11291, I: -11291, U: math.MaxUint64 - 11290},
-	{N: int32(-739910426), F: -739910426, I: -739910426, U: math.MaxUint64 - 739910425},
-	{N: int64(-3177891079421588184), F: -3177891079421588184, I: -3177891079421588184, U: math.MaxUint64 - 3177891079421588183},
-	{N: float32(8.1171875), F: 8.1171875, I: 8, U: 8},
-	{N: float32(-2.9390623569488525), F: -2.9390623569488525, I: -2, U: math.MaxUint64 - 1},
-	{N: float64(1.7), F: 1.7, I: 1, U: 1},
-	{N: NewDecimal(-2, 27115), F: 271.15, I: 271, U: 271},
-	{N: NewDecimal(2, -271), F: -27100, I: -27100, U: math.MaxUint64 - 27099},
-	{N: &Decimal{-8, 12345678}, F: 0.12345678, I: 0, U: 0},
-	{N: &Decimal{8, -123}, F: -123e8, I: -123e8, U: math.MaxUint64 - 123e8 + 1},
+	{N: uint(5), F: 5, I: 5, U: 5, S: "5"},
+	{N: uint8(44), F: 44, I: 44, U: 44, S: "44"},
+	{N: uint16(11290), F: 11290, I: 11290, U: 11290, S: "11290"},
+	{N: uint32(739910425), F: 739910425, I: 739910425, U: 739910425, S: "739910425"},
+	{N: uint64(3177891079421588183), F: 3177891079421588183, I: 3177891079421588183, U: 3177891079421588183, S: "3177891079421588183"},
+	{N: int(-6), F: -6, I: -6, U: math.MaxUint64 - 5, S: "-6"},
+	{N: int8(-45), F: -45, I: -45, U: math.MaxUint64 - 44, S: "-45"},
+	{N: int16(-11291), F: -11291, I: -11291, U: math.MaxUint64 - 11290, S: "-11291"},
+	{N: int32(-739910426), F: -739910426, I: -739910426, U: math.MaxUint64 - 739910425, S: "-739910426"},
+	{N: int64(-3177891079421588184), F: -3177891079421588184, I: -3177891079421588184, U: math.MaxUint64 - 3177891079421588183, S: "-3177891079421588184"},
+	{N: float32(8.1171875), F: 8.1171875, I: 8, U: 8, S: "8.1171875"},
+	{N: float32(-2.9390623569488525), F: -2.9390623569488525, I: -2, U: math.MaxUint64 - 1, S: "-2.9390624"},
+	{N: float64(1.7), F: 1.7, I: 1, U: 1, S: "1.7"},
+	{N: NewDecimal(-2, 27115), F: 271.15, I: 271, U: 271, S: "27115e-2"},
+	{N: NewDecimal(2, -271), F: -27100, I: -27100, U: math.MaxUint64 - 27099, S: "-271e2"},
+	{N: &Decimal{-8, 12345678}, F: 0.12345678, I: 0, U: 0, S: "12345678e-8"},
+	{N: &Decimal{8, -123}, F: -123e8, I: -123e8, U: math.MaxUint64 - 123e8 + 1, S: "-123e8"},
 }
 
 func TestNumericConversion(t *testing.T) {
@@ -46,6 +49,27 @@ func TestNumericConversion(t *testing.T) {
 
 		if u != n.U {
 			t.Errorf("Error converting %#v to uint64: expected %v, got %v", n.N, n.U, u)
+		}
+	}
+}
+
+type testXML struct {
+	XMLName xml.Name `json:"-" xml:"s" codec:"-"`
+	Attr    Numeric  `json:"a,omitempty" xml:"a,attr,omitempty" codec:"a"`
+	Element Numeric  `json:"e,omitempty" xml:"e,omitempty" codec:"e"`
+}
+
+func TestNumericEncoding(t *testing.T) {
+	for _, n := range numericTests {
+		e := fmt.Sprintf(`<s a="%s"><e>%s</e></s>`, n.S, n.S)
+		s := testXML{Attr: n.N, Element: n.N}
+		x, err := xml.Marshal(s)
+		if err != nil {
+			t.Errorf("Error encoding %#v to XML: %s", n.N, err)
+			continue
+		}
+		if string(x) != e {
+			t.Errorf("Error converting %#v to XML: expected %s, got %s", n.N, e, x)
 		}
 	}
 }
